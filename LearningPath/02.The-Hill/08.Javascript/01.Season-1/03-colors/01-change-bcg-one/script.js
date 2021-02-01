6279d3c40acf9c1d484a265715257aeae89370cc
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-01 23:15:15 +0100

    essai 2.6 et 3.1

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/03-colors/01-change-bcg-one/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/03-colors/01-change-bcg-one/script.js
@@ -4,6 +4,33 @@
 
 (() => {
 
+        function red() {
+        document.body.style.backgroundColor = "red";}
+
+            const performOperation = operation => {
+
+                
+                switch (operation) {
+                    case "Red" :
+                        red();
+                        break;
+                    case "green" :
+                        document.body.style.backgroundColor = "green";
+                        break;
+                    case "yellow" :
+                        document.body.style.backgroundColor = "yellow";
+                        break;
+                    case "blue" :
+                        document.body.style.backgroundColor = "blue";
+                        break;
+                }
+
+
+
+            Array.from(document.querySelectorAll("button")).forEach($btn =>
+                $btn.addEventListener(
+                    "click",
+                    () => (performOperation($btn.id), false),
     // your code here
 
 })();
