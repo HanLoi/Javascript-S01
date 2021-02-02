373b4fccb071385339edcae7fd6fef48a1a01d87
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-02 14:11:38 +0100

    4.2

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/04-dates/02-title-by-hour-two/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/04-dates/02-title-by-hour-two/script.js
@@ -7,5 +7,13 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+    var myDate = new Date(); 
+    var x =  myDate.getHours() * 60
+    var y = myDate.getMinutes() + x
+       
+        if (  y <= 1050 ) 
+            {     document.getElementById('target').innerHTML ="Hello"; } 
+        else
+             { document.getElementById('target').innerHTML= "Good Evening!"; } 
 
 })();
