c710bcb8d1606003e1b7c23f20cebcfa3162693a
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-02 09:34:59 +0100

    2.6 fini

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/02-maths/06-fizzbuzz/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/02-maths/06-fizzbuzz/script.js
@@ -5,14 +5,18 @@
 
     var i = new Number(0)
     for (i = 0; i <=100 ; i++) {
-        if (i%3 ==0) {
+
+        if (i%3 == 0) {
             console.log("fizz")
         }
         else if (i%5 == 0){
             console.log("buzz")
         } 
-        else if (i%3==0 && i%5==0){
+        else if (i%3 && i%5) {
             console.log("fizzbuzz")
+        }
+        else {
+            console.log(i)
         }
     }
     console.log(i)
