ae4efda79e5c2f1ce1bec569ade083b374d47dd1
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-03 16:01:50 +0100

    6.1

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/06-objects/01-generate-object/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/06-objects/01-generate-object/script.js
@@ -3,4 +3,10 @@
 
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", () => { 
+        var moi = new Object()
+        var moi = {lastname:"Hannecart",firstname:"Loïc",age:"27",city:"Charleroi",country:"belgium"}
+        
+        console.log("Je me présente. Je m'appel "+ moi.lastname+ " "+ moi.firstname+". J'ai "+moi.age+" et j'habite à "+moi.city+" en "+moi.country)
+    })
 })();
