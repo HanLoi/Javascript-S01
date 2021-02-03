78b9f120d70897f40f6b5adac00520661b2d8c64
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-03 15:25:55 +0100

    5.14

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/05-arrays/14-bird-names-generator/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/05-arrays/14-bird-names-generator/script.js
@@ -31,4 +31,23 @@
     ]);
 
     // your code here
+    document.getElementById("run").addEventListener("click", () => { 
+    
+        document.getElementById("run").addEventListener("click", () => {
+            let bird = birds[Math.floor(Math.random()* (birds.length-1))];
+            let listad = adjectives.values();
+            
+            let adje;
+    
+            for (let index = 0; index < Math.floor(Math.random()* (adjectives.size)); index++) {
+                adje = listad.next().value;
+            }
+            
+            if(bird.fem){
+                document.getElementById("target").innerHTML = "la "+ bird.name + " "  + adje+ "e";
+            }else{
+                document.getElementById("target").innerHTML = "le "+ bird.name + " "+adje ;
+            }
+        })
+    })
 })();
