a374892e04f92228eae7990d005ae6b45f2ed36e
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-04 11:02:07 +0100

    6.3

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/06-objects/03-keys-values/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/06-objects/03-keys-values/script.js
@@ -11,4 +11,9 @@
         status: "married",
     };
     // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        console.log(Object.keys(person))
+        console.log(Object.values(person))
+     })
+    
 })();
