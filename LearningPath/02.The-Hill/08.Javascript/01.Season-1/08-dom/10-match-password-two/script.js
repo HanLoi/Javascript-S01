db482617d7ac2f072312036a29ebf59fc8c354b3
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-05 14:32:22 +0100

    8.12

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/08-dom/10-match-password-two/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/08-dom/10-match-password-two/script.js
@@ -3,6 +3,22 @@
 
 (() => {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", ()=>{
+
+        let one = document.getElementById("pass-one");
+        let two = document.getElementById("pass-two");
+
+        let error = document.createAttribute("class");
+        error.value = "error";
+
+        let error2 = document.createAttribute("class");
+        error2.value = "error";
+
+        if (one.value != two.value) {
+            one.setAttributeNode(error);
+            two.setAttributeNode(error2);
+        }
+
+    }) 
 
 })();
--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/08-dom/12-change-event-input-two/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/08-dom/12-change-event-input-two/script.js
@@ -4,5 +4,29 @@
 (() => {
 
     // your code here
+    function check(param){
+        let n = ["1","2","3","4","5","6","7","8","9","0"];
+        let pass = param.split("");
+        let n1 = false;
+        let n2 = false;
+        param.split("").forEach(element => {
+            if(n.includes(element)){
+                if(n1 == true){
+                    n2 = true;
+                }else{
+                    n1 = true;
+                }
+            }
+        });
+        return ((pass.length >=8 ? true : false) && n2 );
+    }
 
+    document.getElementById("pass-one").addEventListener("input", () =>{
+
+        let value = document.getElementById("pass-one").value;
+
+        if(check(value) ){
+            document.getElementById("validity").innerHTML = "ok";
+        }
+    })
 })();
