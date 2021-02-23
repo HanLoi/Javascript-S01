b18f4d13ecf4b178f99f7ad52d8aaed68fdf4f31
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-23 20:51:58 +0100

    9.2

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/09-misc/02-typewriter-effect/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/09-misc/02-typewriter-effect/script.js
@@ -1,8 +1,26 @@
-// 09-misc/02-typewriter-effect/script.js - 9.2: typewriter effect
-
-
 (() => {
 
-    // your code here
+    let i = 0;
+    let txt = document.getElementById("target").innerHTML; /* The text */
+  
+
+    function suppression() {
+
+        document.getElementById("target").innerHTML = "";
+
+    }
+
+    function typeWriter() {
+
+        if (i < txt.length) {
+            document.getElementById("target").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, 250);
+        }
+
+    }
+
+    suppression();
+    typeWriter();
 
 })();
