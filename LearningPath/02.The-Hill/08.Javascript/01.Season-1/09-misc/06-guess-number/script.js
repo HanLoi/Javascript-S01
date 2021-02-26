3a788bbecb00868828d46738109fbaa4f6678aa3
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-26 17:34:24 +0100

    9.6

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/09-misc/06-guess-number/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/09-misc/06-guess-number/script.js
@@ -3,5 +3,26 @@
 (() => {
 
     // your code here
+    let x = Math.floor(Math.random()*100-1)+1;
+    let z = prompt("Give me your number");
+    let guesses = 0;
+
+    while ( z != x){
+        if (z < x  ) {
+            z = prompt("too low, retry to guess. Your number ?")
+            guesses += 1
+    
+        }else if (z > x) {
+            z = prompt("too hight, retry to guss. Your Number ?")
+            guesses += 1
+        }
+    }
+
+    if ( z == x) {
+        guesses += 1
+        alert("You find the mystery number ! And you needed " + guesses + " try !")
+    }
+
+
 
 })();
