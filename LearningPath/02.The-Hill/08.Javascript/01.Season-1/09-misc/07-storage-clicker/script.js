94495eefcddcb3142c1693eb8ecc7c76a9065676
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-26 18:22:35 +0100

    9.7

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/09-misc/07-storage-clicker/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/09-misc/07-storage-clicker/script.js
@@ -5,4 +5,47 @@
 
     // your code here
 
+    let value;
+
+    if (localStorage.getItem("value") == undefined)
+    {
+        value = 0
+    } else  {
+        value = parseFloat(localStorage.getItem("value"))
+    }
+
+    document.getElementById("target").innerHTML= value;
+
+    document.getElementById("increment").addEventListener("click", () => {
+        value += 1
+        document.getElementById("target").innerHTML= value;
+        localStorage.removeItem("value");
+        localStorage.setItem("value", value);
+    })
+    
+/*(() => {
+
+    window.onload = () => { 
+
+        if (!localStorage.getItem("number")) {
+
+            localStorage.setItem("number", 0);
+
+        }
+
+        let target = document.getElementById('target');
+        target.innerHTML = localStorage.number;
+    }
+
+    document.getElementById("increment").addEventListener('click',() => {
+
+        let number = localStorage.getItem("number");
+        number ++;
+        target.innerHTML = number;
+        localStorage.number = number;
+
+    })
+
+})();*/
+
 })();
