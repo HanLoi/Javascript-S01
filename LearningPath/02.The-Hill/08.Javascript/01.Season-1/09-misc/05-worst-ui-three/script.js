7e5c372ef4550426cf24e45274b6409bf1af7626
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-26 14:37:19 +0100

    9.5

--- LearningPath/02.The-Hill/08.Javascript/01.Season-1/09-misc/05-worst-ui-three/script.js
+++ LearningPath/02.The-Hill/08.Javascript/01.Season-1/09-misc/05-worst-ui-three/script.js
@@ -2,6 +2,24 @@
 
 (() => {
 
-    // your code here
+    let buttons = Array.from(document.querySelectorAll("button"));
+    let inputs = Array.from(document.querySelectorAll("input"));
+
+    buttons.forEach(button => {
+        
+        button.addEventListener("click", () => {
+
+            let data = button.previousElementSibling;
+            let dataMin = parseFloat(data.getAttribute("data-min"));
+            let dataMax = parseFloat(data.getAttribute("data-max"));
+            let value = Math.floor(Math.random() * (dataMax - dataMin) + dataMin);
+
+            data.value = `${value}`.padStart(2, "0");
+
+            document.getElementById("target").innerHTML = "+" + inputs[0].value + inputs[1].value + inputs[2].value + inputs[3].value;
+
+        })
+
+    });
 
 })();
